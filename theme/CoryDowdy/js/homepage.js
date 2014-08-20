b678d93798dd340444e1757552523c4011e36d3d
cdowdy <cdowdy46@gmail.com>
2014-08-20 12:37:52 -0400

    new javascripts

---
+++ theme/CoryDowdy/js/homepage.js
@@ -0,0 +1,24 @@
+/**
+ * Created by Cory on 8/15/2014.
+ */
+/**
+ *
+ *  Web Starter Kit
+ *  Copyright 2014 Google Inc. All rights reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License
+ *
+ */
+//*! SVG4Everybody Jonathan Neal
+//! Github: https://github.com/jonathantneal/svg4everybody
+(function(){"use strict";function i(){t.classList.remove("open");e.classList.remove("open")}function s(){t.classList.toggle("open");e.classList.toggle("open")}var e=document.querySelector(".navdrawer-container");var t=document.querySelector(".app-bar");var n=document.querySelector(".menu");var r=document.querySelector("main");r.addEventListener("ontouchstart",i);r.addEventListener("click",i);n.addEventListener("click",s);e.addEventListener("click",function(e){if(e.target.nodeName==="A"||e.target.nodeName==="LI"){i()}})})();(function(e,t,n,r,i){function s(t,n){if(n){var r=n.getAttribute("viewBox"),i=e.createDocumentFragment(),s=n.cloneNode(true);if(r){t.setAttribute("viewBox",r)}while(s.childNodes.length){i.appendChild(s.childNodes[0])}t.appendChild(i)}}function o(){var t=this,n=e.createElement("x"),r=t.s;n.innerHTML=t.responseText;t.onload=function(){r.splice(0).map(function(e){s(e[0],n.querySelector("#"+e[1].replace(/(\W)/g,"\\$1")))})};t.onload()}function u(){var i;while(i=t[0]){var a=i.parentNode,f=i.getAttribute("xlink:href").split("#"),l=f[0],c=f[1];a.removeChild(i);if(l.length){var h=r[l]=r[l]||new XMLHttpRequest;if(!h.s){h.s=[];h.open("GET",l);h.onload=o;h.send()}h.s.push([a,c]);if(h.readyState===4){h.onload()}}else{s(a,e.getElementById(c))}}n(u)}if(i){u()}})(document,document.getElementsByTagName("use"),window.requestAnimationFrame||window.setTimeout,{},/Trident\/[567]\b/.test(navigator.userAgent))
