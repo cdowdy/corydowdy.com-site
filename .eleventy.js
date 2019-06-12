2821f9471ee528b88f286e34caf05aab4138345a
Cory Dowdy <cdowdy46@gmail.com>
2019-06-12 12:43:38 -0400

    add passthroughcopy for files directory

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,12 @@
-module.exports = {
-
-};
+module.exports = function(eleventyConfig) {
+    // Copy the `files/` directory 
+    eleventyConfig.addPassthroughCopy("files");
+    
+    // Copy the `css/fonts/` directory
+    // If you use a subdirectory, it’ll copy using the same directory structure.
+    // eleventyConfig.addPassthroughCopy("css/fonts");
+    
+    return {
+      passthroughFileCopy: true
+    };
+  };
