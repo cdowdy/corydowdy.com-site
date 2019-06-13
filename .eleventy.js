8153d761d3bf3ecdf66848381e6267da2a561e36
Cory Dowdy <cdowdy46@gmail.com>
2019-06-13 10:27:41 -0400

    move src files to their own directory to cleanup

--- .eleventy.js
+++ .eleventy.js
@@ -4,9 +4,14 @@ module.exports = function(eleventyConfig) {
     
     // Copy the `css/fonts/` directory
     // If you use a subdirectory, it’ll copy using the same directory structure.
-    // eleventyConfig.addPassthroughCopy("css/fonts");
+    // eleventyConfig.addPassthroughCopy("./css");
     
     return {
+
+      dir: {
+        input: "src/site"
+      },
+
       passthroughFileCopy: true
     };
   };
