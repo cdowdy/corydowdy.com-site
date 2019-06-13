398db064cca929c41f972c7638ba4eab0458532f
Cory Dowdy <cdowdy46@gmail.com>
2019-06-13 11:57:31 -0400

    adds css to passthrough to be copied

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,10 @@
 module.exports = function(eleventyConfig) {
     // Copy the `files/` directory 
-    eleventyConfig.addPassthroughCopy("files");
+    eleventyConfig.addPassthroughCopy("src/site/files");
     
     // Copy the `css/fonts/` directory
     // If you use a subdirectory, it’ll copy using the same directory structure.
-    // eleventyConfig.addPassthroughCopy("./css");
+    eleventyConfig.addPassthroughCopy("src/site/css");
     
     return {
 
